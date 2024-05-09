d5f18a64e8e1af8f3137e193a79f0df1b26222b9
Sandeep Tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2024-05-09 21:09:14 +0200

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,12 +1,20 @@
 import pika
+import ssl
 
 # Connection parameters for RabbitMQ on EC2 for producer
-ec2_public_ip = 'YOUR_EC2_PUBLIC_IP'   # Replace 'YOUR_EC2_PUBLIC_IP' with the actual public IP address of your EC2 instance
+#ec2_public_ip = 'YOUR_EC2_PUBLIC_IP'   # Replace 'YOUR_EC2_PUBLIC_IP' with the actual public IP address of your EC2 instance
 credentials = pika.PlainCredentials('myuser', 'mypassword')
-parameters = pika.ConnectionParameters(ec2_public_ip, 5672, '/', credentials)
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+parameters = pika.ConnectionParameters(host='a-25c34e4d-a3eb-32de-abfg-l95d931afc72f.mq.eu-central-1.amazonaws.com',
+                                       port=5671,
+                                       virtual_host='/',
+                                       credentials=credentials,
+                                       ssl_options=pika.SSLOptions(context)
+                                       )
+
 
 connection = pika.BlockingConnection(parameters)
-#connection = pika.BlockingConnection(pika.ConnectionParameters(''amqp://myuser:mypassword@your-ec2-instance-public-ip''))
+
 
 channel = connection.channel()
 
