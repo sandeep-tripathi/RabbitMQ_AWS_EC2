690ec281caef5467b885723eabcec224da38b5d4
Sandeep Tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2024-05-09 21:11:33 +0200

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,9 +1,19 @@
 import pika
+import ssl
 
 # Connection parameters for RabbitMQ on EC2 for consumer
-ec2_public_ip = 'YOUR_EC2_PUBLIC_IP'   # Replace 'YOUR_EC2_PUBLIC_IP' with the actual public IP address of your EC2 instance
-credentials = pika.PlainCredentials('myuser', 'mypassword')   # User named myuser should already be created 
-parameters = pika.ConnectionParameters(ec2_public_ip, 5672, '/', credentials)
+#ec2_public_ip = 'YOUR_EC2_PUBLIC_IP'   # Replace 'YOUR_EC2_PUBLIC_IP'
+credentials = pika.PlainCredentials('myuser', 'mypassword')
+
+#  AWS is using AMQPS, you should add ssl_options to your connection parameters
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+parameters = pika.ConnectionParameters(host='a-25c34e4d-a3eb-32de-abfg-l95d931afc72f.mq.eu-central-1.amazonaws.com',
+                                       port=5671,
+                                       virtual_host='/',
+                                       credentials=credentials,
+                                       ssl_options=pika.SSLOptions(context)
+                                       )
+
 
 # Declare a queue named 'myqueue'
 channel.queue_declare(queue='myqueue')
